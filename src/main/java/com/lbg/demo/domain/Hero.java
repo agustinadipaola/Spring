a2c1a4dd06eb7d37c0175e02651d74e182fe5b02
unknown <agustinadipaolauk@gmail.com>
2024-01-23 15:52:03 +0000

    updates srping

---
+++ src/main/java/com/lbg/demo/domain/Hero.java
@@ -0,0 +1,7 @@
+package com.lbg.demo.domain;
+
+public class Hero {
+	private String name;
+	private String powers;
+
+}
---
+++ src/main/java/com/lbg/demo/rest/HeroController.java
@@ -0,0 +1,13 @@
+package com.lbg.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeroController {
+
+	@GetMapping("/agustina")
+	public String greeting() {
+		return "HELLO HOLA LABAS";
+	}
+}
